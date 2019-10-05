e0edaa5e332c8461797c553f0997a0dc42b57a25
MAwais <mawais54013@outlook.com>
2019-10-05 10:05:29 -0700

    Fixed Game Code

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
