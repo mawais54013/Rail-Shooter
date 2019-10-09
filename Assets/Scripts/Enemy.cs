165de4ce41b9b0c39da3b009a4ecc0c9be4576cc
MAwais <mawais54013@outlook.com>
2019-10-08 22:53:55 -0700

    Added Score+Enemy Death

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
     void Start()
     {
-        
+        AddNonTriggerBoxCollider();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddNonTriggerBoxCollider()
     {
-        
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
     }
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
+
+
+        //var parentscale = gameObject.transform.localScale;
+        //var newobject = Instantiate(deathFX, transform.position, Quaternion.identity);
+        //parentscale = new Vector3(parentscale.x * 0.1f, parentscale.y * 0.1f, parentscale.z * 0.1f);
+        //newobject.transform.localScale = parentscale;
+        //Destroy(gameObject);
     }
 }
